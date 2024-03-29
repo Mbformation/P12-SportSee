6162672a6da69d42322081c91e3637ebc394756a
Mbformation <immvp@hotmail.fr>
2024-03-29 17:33:32 +0100

    feat: :sparkles: Added icon to sidebar

--- front/src/components/sidebar/SideBar.jsx
+++ front/src/components/sidebar/SideBar.jsx
@@ -1,15 +1,16 @@
-import IconTemplate from './iconTemplate.jsx'
-import { Link } from 'react-router-dom';
-import Yoga from './../../assets/icons/yoga.svg';
-import Swimming from './../../assets/icons/swimming.svg';
-import Cycling from './../../assets/icons/cycling.svg';
-import Lifting from './../../assets/icons/lifting.svg';
-import { ReactComponent as HeaderLogo } from './../../assets/logo.svg';
+import { ReactComponent as Yoga} from './../../assets/icons/yoga.svg';
+import { ReactComponent as Swimming } from './../../assets/icons/swimming.svg';
+import { ReactComponent as Cycling } from './../../assets/icons/cycling.svg';
+import { ReactComponent as Lifting } from './../../assets/icons/lifting.svg';
 export default function SideBar() {
   return (
     <div className="sidebar-wrapper">
-      <IconTemplate />
-      <Link to='/'><HeaderLogo /></Link>
+      <ul>
+        <li><Yoga /></li>
+        <li><Swimming /></li>
+        <li><Cycling /></li>
+        <li><Lifting /></li>
+      </ul>
     </div>
   );
 }
