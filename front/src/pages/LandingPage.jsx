ba116253810e6238512db17880c757652023d211
Mbformation <immvp@hotmail.fr>
2024-05-14 14:58:04 +0200

    refactor: :recycle: Made Landing Page more readible

--- front/src/pages/LandingPage.jsx
+++ front/src/pages/LandingPage.jsx
@@ -1,10 +1,15 @@
 import {Link } from 'react-router-dom';
+import { ReactComponent as HeaderLogo } from '../assets/logo.svg';
+
 
 export default function LandingPage() {
     return (
-      <div>
-        <Link to={`/user/12/profile`}>Karl Dovineau</Link>
-        <Link to={`/user/18/profile`}>Cecilia Ratorez</Link>
+      <div className='sign-in'>
+        <HeaderLogo />
+        <div className="sign-in-links">
+          <Link to={`/user/12/profile`}>👉 Karl Dovineau</Link>
+          <Link to={`/user/18/profile`}>👉 Cecilia Ratorez</Link>
+        </div>
       </div>
     );
   }
