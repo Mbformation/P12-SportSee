5e00a7dfa49b074c38daf1e9962ef360e041d7c0
Mbformation <immvp@hotmail.fr>
2024-05-16 15:04:18 +0200

    style: :adhesive_bandage: Removed letter "e" to render profil instead of profile

--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -1,15 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 // Componsant de la barre de navigation horizontale
 export default function NavBar() {
-    return (
-      <nav>
-        <ul>
-          <li><Link to={`home`}>Accueil</Link></li>
-          <li><Link to={`profile`}>Profile</Link></li>
-          <li><Link to={`settings`}>Réglages</Link></li>
-          <li><Link to={`community`}>Communauté</Link></li>
-        </ul>
-      </nav>
-    );
-  }
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to={`home`}>Accueil</Link>
+        </li>
+        <li>
+          <Link to={`profile`}>Profil</Link>
+        </li>
+        <li>
+          <Link to={`settings`}>Réglages</Link>
+        </li>
+        <li>
+          <Link to={`community`}>Communauté</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
